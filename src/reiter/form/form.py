4fc1df24de659ce49c9f91f85109e9185274e242
Souheil CHELFOUH <trollfot@gmail.com>
2021-02-18 13:34:42 +0100

    the Form class is now in the wtforms_pydantic package

--- src/reiter/form/form.py
+++ src/reiter/form/form.py
@@ -3,17 +3,10 @@ import pydantic
 from horseman.meta import Overhead, APIView
 from reiter.view.meta import View
 from reiter.form.meta import FormViewMeta
-from wtforms_pydantic.converter import Converter, model_fields
+from wtforms_pydantic import Form
 
 
-class Form(wtforms.form.BaseForm):
-
-    @classmethod
-    def from_model(cls, model: pydantic.BaseModel,
-                   only=(), exclude=(), **overrides):
-        return cls(Converter.convert(
-            model_fields(model, only=only, exclude=exclude), **overrides
-        ))
+Form  # BBB: backward compatible import.
 
 
 class FormView(View, metaclass=FormViewMeta):
